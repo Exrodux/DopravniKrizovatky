5e416d9d0d70f89c4b44aeb689bf1e49531b07a5
krenekt.07 <krenekt.07@spst.eu>
2025-11-05 23:07:53 +0100

    Testovani PracticeForm

--- DopravniKrizovatky/PracticeForm.Designer.cs
+++ DopravniKrizovatky/PracticeForm.Designer.cs
@@ -29,11 +29,15 @@
         private void InitializeComponent()
         {
             this.btnBack = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lstVehicles = new System.Windows.Forms.ListBox();
+            this.btnLoadScenario = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnBack
             // 
-            this.btnBack.Location = new System.Drawing.Point(12, 12);
+            this.btnBack.Location = new System.Drawing.Point(497, 326);
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(75, 23);
             this.btnBack.TabIndex = 0;
@@ -41,20 +45,64 @@
             this.btnBack.UseVisualStyleBackColor = true;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(60, 62);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(0, 13);
+            this.lblTitle.TabIndex = 1;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(60, 228);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(0, 13);
+            this.lblDescription.TabIndex = 2;
+            // 
+            // lstVehicles
+            // 
+            this.lstVehicles.FormattingEnabled = true;
+            this.lstVehicles.Location = new System.Drawing.Point(344, 95);
+            this.lstVehicles.Name = "lstVehicles";
+            this.lstVehicles.Size = new System.Drawing.Size(185, 134);
+            this.lstVehicles.TabIndex = 3;
+            // 
+            // btnLoadScenario
+            // 
+            this.btnLoadScenario.Location = new System.Drawing.Point(12, 326);
+            this.btnLoadScenario.Name = "btnLoadScenario";
+            this.btnLoadScenario.Size = new System.Drawing.Size(100, 23);
+            this.btnLoadScenario.TabIndex = 4;
+            this.btnLoadScenario.Text = "Načíst scénář";
+            this.btnLoadScenario.UseVisualStyleBackColor = true;
+            this.btnLoadScenario.Click += new System.EventHandler(this.btnLoadScenario_Click);
+            // 
             // PracticeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.btnLoadScenario);
+            this.Controls.Add(this.lstVehicles);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.btnBack);
             this.Name = "PracticeForm";
             this.Text = "PracticeForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.ListBox lstVehicles;
+        private System.Windows.Forms.Button btnLoadScenario;
     }
 }
--- DopravniKrizovatky/PracticeForm.cs
+++ DopravniKrizovatky/PracticeForm.cs
@@ -1,20 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 using System.Windows.Forms;
 
 namespace DopravniKrizovatky
 {
     public partial class PracticeForm : Form
     {
+        private Scenario currentScenario;
+        private Random random = new Random();
+
         public PracticeForm()
         {
             InitializeComponent();
+        }
+
+        private void btnLoadScenario_Click(object sender, EventArgs e)
+        {
+            string folder = "../../scenarios";
+
+            if (!Directory.Exists(folder))
+            {
+                MessageBox.Show("Složka 'scenarios' nebyla nalezena!", "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string[] files = Directory.GetFiles(folder, "*.json");
+            if (files.Length == 0)
+            {
+                MessageBox.Show("Ve složce nejsou žádné scénáře!", "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string randomFile = files[random.Next(files.Length)];
+
+            try
+            {
+                string json = File.ReadAllText(randomFile);
+                currentScenario = JsonSerializer.Deserialize<Scenario>(json,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                lblTitle.Text = currentScenario.Title;
+                lblDescription.Text = currentScenario.Description;
+
+                lstVehicles.Items.Clear();
+                foreach (var v in currentScenario.Vehicles)
+                {
+                    lstVehicles.Items.Add($"{v.Id} – {v.Type} ({v.Approach}, {v.Direction})");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Chyba při načítání scénáře:\n" + ex.Message, "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void btnBack_Click(object sender, EventArgs e)
